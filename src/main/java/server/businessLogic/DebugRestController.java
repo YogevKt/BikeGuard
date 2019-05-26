2541840dd75a893b6cdd8f12bd5648dadef916ec
Yogev Ktuvim <yogev214@gmail.com>
2019-05-26 19:34:50 +0300

    edited debug controller functions

--- src/main/java/server/businessLogic/DebugRestController.java
+++ src/main/java/server/businessLogic/DebugRestController.java
@@ -1,8 +1,7 @@
 package server.businessLogic;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 import server.entities.Area;
 import server.entities.GpsCoords;
 import server.entities.Intersection;
-import server.entities.Location;
 import server.entities.User;
 
 @RestController
@@ -19,110 +17,49 @@ public class DebugRestController {
 	@Autowired
 	private ServerFacade serverFasade;
 
-	@RequestMapping(value = "debug", method = RequestMethod.GET)
+	@RequestMapping(value = "initDB", method = RequestMethod.GET)
 	public void debug() {
 
 		try {
 
 			// add area
-			
-			 Area area1 = new Area(32.113068,34.81804,22); //Afeka
-			 serverFasade.addArea(area1);
-			 Area area2 = new Area(32.124555,34.855949,21); //Morasha
-			 serverFasade.addArea(area2);
-			
-			 
-			// add intersection nearby yogev's home
-			  serverFasade.addIntersection(new Intersection(32.113622,34.816944,22));//Afeka intersection
-			  serverFasade.addIntersection(new Intersection(32.119251,34.810396,22));//KKL intersection
-			  
-			  
-			// add driver
-			User user1 = new User(
-					"clX5g_VUGCU:APA91bGhM-A_a8C_nGHCi1-KkleO40Zt9k3X9v1fx58zmLI8oS3e1_1bQrToPqqq1dRniPHIekjzCS9MYUHIp_-k8pRWzUzwwnsMhpOqrlk45mtkcjyew0XaTm0wtdjcFWSBZmJbOITr",
-					User.UserType.DRIVER, new GpsCoords(32.113604, 34.817115,22));
-			user1.setSpeed(20);
-			
-			serverFasade.setUser(user1);
-			User user2 = new User(
-					"fAfBRGDJpf8:APA91bELeW-CBV1fJm8SqS8H-cDW7Gn7VX1yeAW0xyWpkDfECIcQSzCa0-2rP52D3NTDqIGhTT_TgYSapeGaEX7ks5WIfGlZO-dDvuRzfqAEjRNmfBNv3ToIj_l90eTkt0E08-ekQmL1",
-					User.UserType.BIKER, new GpsCoords(32.113504, 34.818458,22));
-			user2.setSpeed(20);
-			serverFasade.setUser(user2);
+
+			Area area1 = new Area(32.113068, 34.81804, 22); // Afeka
+			serverFasade.addArea(area1);
+			Area area2 = new Area(32.124555, 34.855949, 21); // Morasha
+			serverFasade.addArea(area2);
+
+			// add intersection nearby Afeka
+			serverFasade.addIntersection(new Intersection(32.113622, 34.816944, 22));// Afeka intersection
+			serverFasade.addIntersection(new Intersection(32.119251, 34.810396, 22));// KKL intersection
 
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 
-	@RequestMapping(value = "debug2", method = RequestMethod.GET)
-	public void debugUpdateUsers() throws Exception {
-		
-		User user1 = new User(
-				"clX5g_VUGCU:APA91bGhM-A_a8C_nGHCi1-KkleO40Zt9k3X9v1fx58zmLI8oS3e1_1bQrToPqqq1dRniPHIekjzCS9MYUHIp_-k8pRWzUzwwnsMhpOqrlk45mtkcjyew0XaTm0wtdjcFWSBZmJbOITr",
-				User.UserType.DRIVER, new GpsCoords(32.11334, 34.817512,22));
-		user1.setSpeed(30);
-		serverFasade.setUser(user1);
-		
-		User user2 = new User(
-				"fAfBRGDJpf8:APA91bELeW-CBV1fJm8SqS8H-cDW7Gn7VX1yeAW0xyWpkDfECIcQSzCa0-2rP52D3NTDqIGhTT_TgYSapeGaEX7ks5WIfGlZO-dDvuRzfqAEjRNmfBNv3ToIj_l90eTkt0E08-ekQmL1",
-				User.UserType.BIKER, new GpsCoords(32.113309, 34.818043,22));
-		user2.setSpeed(30);
-		serverFasade.setUser(user2);
-	}
-
-	@RequestMapping(value = "testDistance", method = RequestMethod.GET)
-	public String testDistance() throws Exception {
-		StringBuilder sb = new StringBuilder();
-		User user1 = new User(
-				"clX5g_VUGCU:APA91bGhM-A_a8C_nGHCi1-KkleO40Zt9k3X9v1fx58zmLI8oS3e1_1bQrToPqqq1dRniPHIekjzCS9MYUHIp_-k8pRWzUzwwnsMhpOqrlk45mtkcjyew0XaTm0wtdjcFWSBZmJbOITr",
-				User.UserType.DRIVER, new GpsCoords(32.113604, 34.817115,22));
-
-		User user2 = new User(
-				"fAfBRGDJpf8:APA91bELeW-CBV1fJm8SqS8H-cDW7Gn7VX1yeAW0xyWpkDfECIcQSzCa0-2rP52D3NTDqIGhTT_TgYSapeGaEX7ks5WIfGlZO-dDvuRzfqAEjRNmfBNv3ToIj_l90eTkt0E08-ekQmL1",
-				User.UserType.BIKER, new GpsCoords(32.113504, 34.818458,22));
-
-		List<Intersection> intersections = serverFasade.getIntersections();
-		sb.append("User coords: " + user1.getCoords() + "\n");
-		System.out.println();
-		for (Intersection intersection : intersections) {
-			sb.append("Intersection coords: " + "[" + intersection.getLatitude() + "," + intersection.getLongitude()
-					+ "]" + "\n");
-			sb.append(Location.distance(intersection, user1.getCoords()) + "\n");
-		}
-
-		sb.append("\n\n");
-
-		sb.append("User coords: " + user2.getCoords() + "\n");
-		for (Intersection intersection : intersections) {
-			sb.append("Intersection coords: " + "[" + intersection.getLatitude() + "," + intersection.getLongitude()
-					+ "]" + "\n");
-			sb.append(Location.distance(intersection, user2.getCoords()) + "\n\n");
-		}
-
-		return sb.toString();
-	}
-
-	@RequestMapping(value = "testConversion", method = RequestMethod.GET)
-	public CartesianCoord testConversion() {
-		/*debug();
+	@RequestMapping(value = "addIntersection", method = RequestMethod.POST)
+	public void addIntersection(@RequestBody Intersection intersection) {
 		try {
-			debugUpdateUsers();
+			serverFasade.addIntersection(intersection);
 		} catch (Exception e) {
-			e.printStackTrace();
-		}*/
-		StringBuilder sb = new StringBuilder();
+			System.err.println(e.getMessage());
+		}
+	}
+
+	@RequestMapping(value = "testCollision", method = RequestMethod.GET)
+	public void testConversion() {
+
 		User user1 = new User(
 				"dWv1T0x_mB4:APA91bGTP78G8e7MnQTT8aIdfy2nLxAoXR2YS8_VO3Dgr_fx2Jpkl4PxaQ_3WS5nKtwPix8gFAXlwUkdS61SF8Z9Ugn-FaMhvCUMA2KAPZYQEQJv91I0_O3NdlTufo3guQ7Cgo2IBREa",
-				User.UserType.DRIVER, new GpsCoords(32.113604, 34.817115,22));
+				User.UserType.DRIVER, new GpsCoords(32.113604, 34.817115, 22));
 		user1.setSpeed(20);
-		user1.setCoords(new GpsCoords(32.11334, 34.817512,22));
+		user1.setCoords(new GpsCoords(32.11334, 34.817512, 22));
 
 		User user2 = new User(
 				"fAfBRGDJpf8:APA91bELeW-CBV1fJm8SqS8H-cDW7Gn7VX1yeAW0xyWpkDfECIcQSzCa0-2rP52D3NTDqIGhTT_TgYSapeGaEX7ks5WIfGlZO-dDvuRzfqAEjRNmfBNv3ToIj_l90eTkt0E08-ekQmL1",
-				User.UserType.BIKER, new GpsCoords(32.113504, 34.818458,22));
-		user2.setCoords(new GpsCoords(32.113309, 34.818043,22));
+				User.UserType.BIKER, new GpsCoords(32.113504, 34.818458, 22));
+		user2.setCoords(new GpsCoords(32.113309, 34.818043, 22));
 		user2.setSpeed(20);
 
 		try {
@@ -131,10 +68,44 @@ public class DebugRestController {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		//CartesianCoord x = Location.calculateIntersectionPoint(user1, user2);
-		//System.err.println(x);
 
-		return null;
+	}
+
+	@RequestMapping(value = "temp", method = RequestMethod.POST)
+	public void temp() {
+		try {
+			Area area1 = new Area(32.267527, 34.911479, 43); // Yogev
+			serverFasade.addArea(area1);
+			Area area2 = new Area(31.968026, 34.801616, 50); // May
+			serverFasade.addArea(area2);
+			
+			serverFasade.addIntersection(new Intersection(31.968102, 34.80165, 50));//May's intersection
+			serverFasade.addIntersection(new Intersection(32.267595, 34.911456, 43));//Yogev's intersection
+			
+
+		} catch (Exception e) {
+		}
+	}
+	
+	
+	@RequestMapping(value = "fakeDrivers", method = RequestMethod.POST)
+	public void voidFakeUsers() {
+		//may
+		User maysArea = new User(
+				"dSv1T0x_mB4:APA91bGTP78G8e7MnQTT8aIdfy2nLxAoXR2YS8_VO3Dgr_fx2Jpkl4PxaQ_3WS5nKtwPix8gFAXlwUkdS61SF8Z9Ugn-FaMhvCUMA2KAPZYQEQJv91I0_O3NdlTufo3guQ7Cgo2IBREa",
+				User.UserType.DRIVER, new GpsCoords(31.968086, 34.801468, 50));
+		maysArea.setSpeed(30);
+		maysArea.setCoords(new GpsCoords(31.968088, 34.801533, 50));
+		
+		
+		
+		//yogev
+		User yogevsArea = new User(
+				"dSA1T0x_mB4:APA91bGTP78G8e7MnQTT8aIdfy2nLxAoXR2YS8_VO3Dgr_fx2Jpkl4PxaQ_3WS5nKtwPix8gFAXlwUkdS61SF8Z9Ugn-FaMhvCUMA2KAPZYQEQJv91I0_O3NdlTufo3guQ7Cgo2IBREa",
+				User.UserType.DRIVER, new GpsCoords(32.267537, 34.911343, 43));
+		yogevsArea.setSpeed(30);
+		yogevsArea.setCoords(new GpsCoords(32.267559, 34.911412, 43));
+		
 	}
 
 }
